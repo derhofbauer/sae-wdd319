193c91a0bbc3ad43e01bcee1fb30f996df956324
Alexander Hofbauer <hofbauer.alexander@gmail.com>
2020-04-30 20:35:52 +0200

    [FEATURE] Config helper
    Signed-off-by: Alexander Hofbauer <hofbauer.alexander@gmail.com>

---
+++ mvc/core/Helpers/Config.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Core\Helpers;
+
+class Config
+{
+
+    /**
+     * @param string $configString z.b. "database.host"
+     * @param null   $default
+     *
+     * @return mixed
+     */
+    public static function get ($configString = '', $default = null)
+    {
+        if (strlen($configString) >= 3) {
+            $filename = explode('.', $configString)[0];
+            $key = explode('.', $configString)[1];
+
+            /**
+             * Config file dynamisch laden
+             */
+            $config = require_once __DIR__ . "/../../config/$filename.php";
+
+            if (array_key_exists($key, $config)) {
+                return $config[$key];
+            } else {
+                return $default;
+            }
+        }
+    }
+
+}
