9f2b443da3eb825d278f4150fce6039130dfbead
Alexander Hofbauer <hofbauer.alexander@gmail.com>
2020-05-26 21:34:12 +0200

    [FEATURE] load CSS properly

--- mvc/resources/views/partials/header.php
+++ mvc/resources/views/partials/header.php
@@ -5,9 +5,11 @@
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Custom Shop</title>
+
+    <!-- Der base-Tag muss über CSS & JS Includes stehen, damit die Pfade korrekt berechnert werden -->
     <base href="<?php echo \Core\Helpers\Config::get('app.baseUrl'); ?>">
 
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="public/css/bootstrap.min.css">
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
